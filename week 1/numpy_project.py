9458803eca1baf1cc65697ca7f5e5b522162eb55
Prabesh-Khadka <akhadka115@gmail.com>
2025-08-03 10:05:03 +1200

    numpy_classs project assesment

---
+++ week 1/numpy_project.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+Temperature_celius = np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])
+
+Avg_temp = np.mean(Temperature_celius)
+
+max_value = np.max(Temperature_celius)
+
+min_value = np.min(Temperature_celius)
+
+fahrenheit = Temperature_celius * 9/5 +32
+
+Days = np.where(Temperature_celius > 20)
+
+#for the average temperature
+print("The avareage temperature is:", Avg_temp)
+
+#List all the array present
+print("Temperature given", Temperature_celius)
+
+#for the max value
+print("The maximun Temperature in given array is:", max_value)
+
+#for the minimun value
+print("The minimun temperature in teh array is:", min_value)
+
+#for the fahrenheit
+print("The converted temperatue are:", fahrenheit)
+
+#printing the value for the temperature
+print("The days where the temperature execeeds 20:", Days[0])
