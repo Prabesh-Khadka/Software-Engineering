8b0c759a062edbc30e8a89cb77bf94cac9f183f6
Prabesh-Khadka <akhadka115@gmail.com>
2025-08-02 21:28:52 +1200

    Update Sum_odd_print_evens.py

--- week 1/Sum_odd_print_evens.py
+++ week 1/Sum_odd_print_evens.py
@@ -8,6 +8,7 @@ def Sum_odd_and_print_evens():
 
     odd_sum = 0 # initialize a variable to store the sum of odd  numbers
     even_numbers = [] # initialize to keep track of the even numbers
+    even_sums = 0 # intializing the sum of evens as well
 
     # loop for all the numbers from 1 to number
     for i in range(1, num+1):
@@ -15,12 +16,16 @@ def Sum_odd_and_print_evens():
             odd_sum += i
         else:
             even_numbers.append(i)
+            even_sums += i
 
     # After the loop print all the even numbers found to the sum
     print("Even numbers are:", even_numbers)
 
     #After the total sum of the odd numbers
     print("Sum of total odd numbers are:", odd_sum)
+
+    #the sum of total even numbers as well
+    print("Sum of total even numbers are:", even_sums)
 
 #standard python format
 if __name__ == "__main__":
